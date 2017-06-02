faa439df7c25593b048846fdabe29e3930b77f50
tako <rina.m.takase@gmail.com>
2017-06-02 14:06:43 +0900

    Create base of RecipeEditorUI

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 
-render(<div>hello, electron and react jsx</div>, document.getElementById("app"));
+import RecipeEditorUI from "./components/RecipeEditorUI";
+
+render(<RecipeEditorUI />, document.getElementById("app"));
---
+++ src/renderer/components/RecipeEditorUI.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Editor from "./Editor"
+
+export default class RecipeEditorUI extends React.Component {
+  render() {
+    return (
+      <div>
+        <Editor />
+      </div>
+    );
+  }
+}

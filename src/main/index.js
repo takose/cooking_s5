6daed0f4adf41f53ba85c0638d5d99c72711b9cf
tako <rina.m.takase@gmail.com>
2017-06-13 20:18:49 +0900

    Introduce electron dev tools

--- src/main/index.js
+++ src/main/index.js
@@ -3,8 +3,23 @@ import createMainWindow from "./createMainWindow";
 
 let mainWindow = null;
 
-app.on("ready", () => {
+app.on("ready", async () => {
   mainWindow = createMainWindow();
+  if (process.env.NODE_ENV !== 'development') {
+    const install = () => {
+      const {
+        default: installer,
+        REACT_DEVELOPER_TOOLS,
+      } = require('electron-devtools-installer')
+      const extensions = [
+        REACT_DEVELOPER_TOOLS,
+      ]
+
+      return Promise.all(extensions.map(ext => installer(ext, true)))
+        .catch(console.log)
+    }
+    await install()
+  }
 });
 
 app.on("window-all-closed", () => {

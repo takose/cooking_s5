0075fec651a2781f9a7d824f16872c4ba9c04d4a
tako <rina.m.takase@gmail.com>
2017-06-23 09:15:53 +0900

    Add style

---
+++ src/renderer/components/TableStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+const TableStyle = styled.table`
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-collapse: collapse;
+  border-radius: 6px;
+  width: 100%;
+`
+
+export default TableStyle
